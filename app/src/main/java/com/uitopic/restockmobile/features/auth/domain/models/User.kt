3b66f85b9eb6a60be4c99896c84dde3108703e22
GabrielaShapiama <137426299+GabrielaShapiama28@users.noreply.github.com>
2025-11-03 06:29:35 -0500

    feat: add profile attribute to user data class.

--- app/src/main/java/com/uitopic/restockmobile/features/auth/domain/models/User.kt
+++ app/src/main/java/com/uitopic/restockmobile/features/auth/domain/models/User.kt
@@ -1,8 +1,11 @@
 package com.uitopic.restockmobile.features.auth.domain.models
 
+import com.uitopic.restockmobile.features.profiles.domain.models.Profile
+
 data class User(
     val id: Int,
     val username: String,
     val roleId: Int,
-    val token: String? = null
+    val token: String? = null,
+    val profile: Profile? = null
 )
